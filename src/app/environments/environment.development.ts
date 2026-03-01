4ed0e074c81b717a8d15ece3e9098b24bd9699c3
Ahmed Rabie <arabeea7104@gmail.com>
2026-03-01 23:02:58 +0200

    feat: implement error handling with interceptor and configure API service

---
+++ src/app/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://express-e-commerce-xi.vercel.app',
+};
---
+++ src/app/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://express-e-commerce-xi.vercel.app',
+};
