bac959ee2978f6c9ac8abf1c5c2e0160eb998f02
unknown <modrsh2002@gmail.com>
2026-03-03 01:15:44 +0200

    fix: bug in app.router after solving conflicts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,19 +4,19 @@ export const routes: Routes = [
     {
         path: 'cart',
         loadComponent: () => import('./features/cart/cart/cart').then(m => m.Cart)
-    }
-  {
-    path: 'admin',
-    loadComponent: () =>
-      import('./features/admin/admin-layout/admin-layout').then((com) => com.AdminLayout),
-    children: [
-      {
-        path: 'dashboard',
+    },
+    {
+        path: 'admin',
         loadComponent: () =>
-          import('./features/admin/dashboard/dashboard.component').then(
-            (com) => com.DashboardComponent,
-          ),
-      },
-    ],
-  },
+            import('./features/admin/admin-layout/admin-layout').then((com) => com.AdminLayout),
+        children: [
+            {
+                path: 'dashboard',
+                loadComponent: () =>
+                    import('./features/admin/dashboard/dashboard.component').then(
+                        (com) => com.DashboardComponent,
+                    ),
+            },
+        ],
+    },
 ];
