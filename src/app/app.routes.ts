d02050fcfa63f60c2ca558aca846e409fcf285f1
Ahmed Rabie <arabeea7104@gmail.com>
2026-03-03 02:51:28 +0200

    feat: implement admin dashboard layout and functionality
    - Added routing for admin dashboard with lazy loading.
    - Created AdminLayout component for the admin section.
    - Developed AdminService to fetch dashboard statistics.
    - Introduced AdminStats model for type safety.
    - Created DashboardComponent to display admin statistics.
    - Added HTML structure for admin layout and dashboard.
    - Integrated Tailwind CSS for styling.
    - Removed placeholder content from app.html.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./features/admin/admin-layout/admin-layout').then((com) => com.AdminLayout),
+    children: [
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./features/admin/dashboard/dashboard.component').then(
+            (com) => com.DashboardComponent,
+          ),
+      },
+    ],
+  },
+];
---
+++ src/app/features/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { AdminService } from '../admin';
+
+@Component({
+  selector: 'app-dashboard.component',
+  imports: [],
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent implements OnInit {
+  admin = inject(AdminService);
+  ngOnInit(): void {
+    this.admin.loadDashboardStats();
+  }
+}
