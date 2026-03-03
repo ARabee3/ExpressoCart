03b8f52d061550e5978d3b744b74445754227b89
unknown <modrsh2002@gmail.com>
2026-03-03 02:01:49 +0200

    feat: implement guest session logic with cart module

--- src/app/core/interceptors/session/session-interceptor.ts
+++ src/app/core/interceptors/session/session-interceptor.ts
@@ -1,5 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const sessionInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  if (!req.url.includes('/cart')) return next(req);
+
+  //if (authService.isLoggedIn()) return next(req);
+
+  const sessionId = localStorage.getItem('guest_session_id');
+
+  //if first time to add to cart
+  if (!sessionId) return next(req);
+
+  const cloned = req.clone({
+    setHeaders: { 'x-session-id': sessionId },
+  });
+
+  return next(cloned);
 };
