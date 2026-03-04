db4e7871de4057f69945500509de5713d5d4551f
jowael <yousefwail22@gmail.com>
2026-03-04 22:18:26 +0200

    refactor: Update app configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastService } from './core/services/toast.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('ExpressoCart');
+  public readonly toastService = inject(ToastService);
 }
